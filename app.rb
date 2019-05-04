6c177e094e924711aa87e912094abee39b6e163f
luis quilho <luis.quilho@gmail.com>
2019-05-04 17:02:43 +0100

    finished sinatra 101

--- app.rb
+++ app.rb
@@ -2,11 +2,14 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+set :bind, '0.0.0.0'
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+#ohhhh, so these are the routs!!"
 get '/' do
   erb :index
 end
@@ -15,12 +18,9 @@ get '/about' do
   erb :about
 end
 
-# get '/team' do
-#   return "HELLLLLLLLLLLO WORLD!!!!!!!!"
-# end
-
 get '/team/:username' do
   binding.pry
   puts params[:username]
   "The username is #{params[:username]}"
 end
+
